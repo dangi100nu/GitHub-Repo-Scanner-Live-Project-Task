ab628b59be75e6094beb7d78a8c06b9cc1077a47
sonu dangi <dangisonu100@gmail.com>
2024-07-09 15:49:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(
   {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://git-hub-repo-scanner-live-project-task-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
