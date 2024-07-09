7e93c913771638449180abe72a772bbb20de0280
sonu dangi <dangisonu100@gmail.com>
2024-07-09 15:43:05 +0530

    Update index.js

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://github-repo-scanner-live-project-task.vercel.app",
   cache: new InMemoryCache({}),
 });
 
